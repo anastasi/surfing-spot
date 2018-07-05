43b83358ed7ff4224e3834dbcdb3d3cb42bc9fc5
Anastasia Yanvareva <anastasia.yanvareva@gmail.com>
2018-07-05 08:59:00 +0300

    Added images

--- src/containers/Trips.js
+++ src/containers/Trips.js
@@ -1,20 +1,45 @@
 import React, { Component } from 'react';
-import Trip from './Trip';
 import { inject, observer } from 'mobx-react';
 import { Link } from "react-router-dom";
+
+const getBase64 = (file) => {
+  return new Promise((resolve,reject) => {
+     const reader = new FileReader();
+     reader.onload = () => resolve(reader.result);
+     reader.onerror = error => reject(error);
+     reader.readAsDataURL(file);
+  });
+}
 
 @inject('TripStore')
 @observer
 class Trips extends Component {
+  state = {
+    selectedFile: null
+  }
+
   handleSubmit = e => {
     e.preventDefault();
-    this.props.TripStore.addTrip(this.trip.value);
+    this.props.TripStore.addTrip({
+      name: this.trip.value,
+      img: this.state.selectedFile
+    });
     e.target.reset();
   }
+  fileChangedHandler = e => {
+    
+    const file = e.target.files[0];
+    getBase64(file).then(base64 => {
+      this.setState({
+        selectedFile: base64
+      })
+      console.debug("file stored",base64);
+    });
+  } 
 
   render() {
     const { TripStore } = this.props;
-
+    console.log(TripStore);
     return (
       <div>
         <form onSubmit={e => this.handleSubmit(e)}>
@@ -23,13 +48,23 @@ class Trips extends Component {
             ref={input => (this.trip = input)}
             placeholder="Add a trip"
           />
+          <input 
+            type="file" 
+            onChange={this.fileChangedHandler}
+            ref={input => (this.img = input)}
+          />
+
           <button>Add trip</button>
+          
         </form>
+
         <h2>You have {TripStore.tripCount} trips</h2>
-        <ul>{TripStore.trips.map((trip,i) => <li key={i}><Link to={{ pathname: `/trip/${trip}`}}>{trip}</Link></li>)}</ul>
+        <ul>{TripStore.trips.map((trip,i) => <li key={i}>
+          <Link to={{ pathname: `/trip/${trip.name}`}}>{trip.name}
+            <img src={trip.img}/>
+          </Link></li>)}</ul>
       </div>
     )
-
   }
 }
 
